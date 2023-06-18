301ed6321a09466a55713bd0e93f53b89a4dce2f
James <diagjames@gmail.com>
2023-06-18 19:13:29 -0300

    more cruds

--- src/main/java/br/com/bantads/authms/rest/AuthREST.java
+++ src/main/java/br/com/bantads/authms/rest/AuthREST.java
@@ -1,11 +1,16 @@
 package br.com.bantads.authms.rest;
 
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,9 +42,26 @@ public class AuthREST {
 		}
 	}
 	
-	@GetMapping("/")
-	public String getString() {
-		return "Ola";
-	}
+	@PutMapping("/auth")
+    public ResponseEntity<AuthDTO> alterarLogin(@RequestBody AuthDTO auth) {
+        throw new UnsupportedOperationException();
+    }
+
+    @DeleteMapping("/auth/{id}")
+    public ResponseEntity<AuthDTO> removerLogin(@PathVariable("id") int id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @GetMapping("/auth/{id}")
+    public ResponseEntity<AuthDTO> buscarLogin(@PathVariable("id") int id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @GetMapping("/auth")
+    public ResponseEntity<List<AuthDTO>> buscarTodosLogins(@PathVariable("id") int id) {
+        throw new UnsupportedOperationException();
+    }
+	
+	
 
 }
